d077f627ca1631494ff5368511103bb17ecc025f
= <mpretty@voceconnect.com>
2015-07-13 15:50:30 -0400

    fixing bug with wrong has_action function

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 if( defined( 'ABSPATH' ) && function_exists('add_action') ) {
-	if(!action_exists('admin_init', array( 'Voce_Color_Setting', 'initialize' ) ) ) {
+	if(!has_action('admin_init', array( 'Voce_Color_Setting', 'initialize' ) ) ) {
 		add_action( 'admin_init', array( 'Voce_Color_Setting', 'initialize' ) );
 	}
 }
