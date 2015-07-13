d3636a39ec34778b74e7a1ffb6dbd3d18c8eb21a
= <mpretty@voceconnect.com>
2015-07-13 15:27:32 -0400

    adding bootstrap files

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+function vs_display_color_picker( $value, $setting, $args ) {
+	return Voce_Color_Setting::display_color_picker( $value, $setting, $args );
+}
+
+function vs_sanitize_color_picker( $value, $setting, $args ) {
+	return Voce_Color_Setting::sanitize_color_picker( $value, $setting, $args );
+}
